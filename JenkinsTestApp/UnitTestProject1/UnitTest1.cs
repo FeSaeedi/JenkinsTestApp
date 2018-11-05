7fa6d24098664b95037c135f08f1aac7e06ea8a7
FeSaeedi <f.saeedi85@yahoo.com>
2018-11-05 11:01:15 +0330

    unittest

---
+++ JenkinsTestApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testclass = new  JenkinsTestApp.Models.TestClass();
+            //assert
+            int expected = testclass.GetValue();
+            int actual = 5;
+           Assert.AreEqual(expected, actual, "fail");
+        }
+    }
+    
+}
