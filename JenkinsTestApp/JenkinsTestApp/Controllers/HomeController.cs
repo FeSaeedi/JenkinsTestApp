5920a8fb232aa91bcaf32b686cc493ac8b8892cd
FeSaeedi <f.saeedi85@yahoo.com>
2018-11-05 13:05:08 +0330

    test2

--- JenkinsTestApp/JenkinsTestApp/Controllers/HomeController.cs
+++ JenkinsTestApp/JenkinsTestApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace JenkinsTestApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page1";
+            ViewBag.Title = "Home Page";
 
             return View();
         }
